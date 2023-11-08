f04d94d42da2b94ee9f2e950c6033fb10918bf2d
ribeirosamuel848@gmail.com <ribeirosamuel848@gmail.com>
2023-11-07 22:25:45 -0300

    a1

---
+++ data/cmdplugin.js
@@ -0,0 +1,16 @@
+//CMD PLUGIN
+
+
+// PACK VAR
+var main = document.getElementsByTagName("body")[0];
+
+// GEMERATOR CANVAS SPACE
+
+function generatorCanvas(){
+  
+  let canvas = document.createElement("canvas");
+  main.appendChild( canvas );
+  
+}
+
+generatorCanvas();
